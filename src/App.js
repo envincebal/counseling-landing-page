cf405a08269e581f5f57c8f23c6b380dee11dbf1
envincebal <vincentyan1@gmail.com>
2021-12-08 20:10:28 -0800

    changed to browser routing.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {  HashRouter as Router, Switch, Route} from "react-router-dom";
+import {  BrowserRouter, Switch, Route} from "react-router-dom";
 import NavBar from "./components/Navbar/Navbar";
 import HomePage from "./components/HomePage/HomePage";
 import AboutPage from "./components/AboutPage/AboutPage";
@@ -15,22 +15,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <Router basename="/">
+      <BrowserRouter>
       <NavBar/>
       <div className="app-container">
         <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/about" component={AboutPage} />
-        <Route exact path="/emdr" component={EMDRPage} />
-        <Route exact path="/fbcc" component={FBCCPage} />
-        <Route exact path="/ppd" component={PPDPage} />
-        <Route exact path="/tfcbt" component={TFCBTPage} /> <Route exact path="/rates" component={RatesPage} />
-        <Route exact path="/contact" component={ContactPage} />
+        <Route path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/emdr" component={EMDRPage} />
+        <Route path="/fbcc" component={FBCCPage} />
+        <Route path="/ppd" component={PPDPage} />
+        <Route path="/tfcbt" component={TFCBTPage} /> <Route path="/rates" component={RatesPage} />
+        <Route path="/contact" component={ContactPage} />
       </Switch>
       </div>
 
       <Footer />
-      </Router>
+      </BrowserRouter>
     </div>
   );
 }
