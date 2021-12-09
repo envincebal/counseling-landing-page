c1f4bf20fee805a486a86b0e43c1f601c55f0b24
envincebal <vincentyan1@gmail.com>
2021-12-08 20:42:08 -0800

    Hash browser

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {  BrowserRouter, Switch, Route} from "react-router-dom";
+import {  HashRouter as Router, Switch, Route} from "react-router-dom";
 import NavBar from "./components/Navbar/Navbar";
 import HomePage from "./components/HomePage/HomePage";
 import AboutPage from "./components/AboutPage/AboutPage";
@@ -15,23 +15,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Router>
       <NavBar/>
       <div className="app-container">
         <Switch>
-        <Route path="/" component={HomePage} />
+        <Route exact path="/" component={HomePage} />
         <Route exact path="/about" component={AboutPage} />
         <Route exact path="/emdr" component={EMDRPage} />
         <Route exact path="/fbcc" component={FBCCPage} />
         <Route exact path="/ppd" component={PPDPage} />
-        <Route exact path="/tfcbt" component={TFCBTPage} /> 
-        <Route exact path="/rates" component={RatesPage} />
+        <Route exact path="/tfcbt" component={TFCBTPage} /> <Route exact path="/rates" component={RatesPage} />
         <Route exact path="/contact" component={ContactPage} />
       </Switch>
       </div>
 
       <Footer />
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
